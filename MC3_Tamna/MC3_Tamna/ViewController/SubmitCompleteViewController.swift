126aaa561d863c3f9cb79d8c631dbc7aaf1c6d2a
MBSoo <qud9783@khu.ac.kr>
2022-07-19 00:28:22 +0900

    Add: SubmitCompleteViewController에 StarView로 진행상황을 넘겨줄 수 있는 버튼을 만들었습니다.
    진행상황을 넘겨주는 버튼을 만들었습니다만... 버그 발생 ! 컴파일 에러는 뜨지 않지만... 원하는 대로 작동하지를 않습니다 ㅜ
    
    #BUG
    func didClearQuizID 함수가 제대로 동작하지 않습니다.. navigation을 원하는 viewController로 나갈 수 있게끔 하는 코드를 추가해서 인 것 같긴 한데.. 제가 짠 코드가 아니라 어떻게 수정해야 할 지 모르겠습니다... 내일 같이 해요..!

--- MC3_Tamna/MC3_Tamna/ViewController/SubmitCompleteViewController.swift
+++ MC3_Tamna/MC3_Tamna/ViewController/SubmitCompleteViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class SubmitCompleteViewController: UIViewController {
     
     var quiz: Quiz?
+    var delegate: QuizDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemBackground
         view.addSubview(successImage)
         view.addSubview(success)
+        view.addSubview(endButton)
         success.text = """
             정답은: \(quiz?.answer[quiz?.rightAnswerIndex ?? 0] ?? "")
             돌고래 너무 귀엽죠 ~~
@@ -33,10 +35,17 @@ class SubmitCompleteViewController: UIViewController {
             success.widthAnchor.constraint(equalToConstant: view.frame.width - 100),
             success.heightAnchor.constraint(equalToConstant: 40 * 4)
         ])
+        NSLayoutConstraint.activate([
+            endButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            endButton.centerYAnchor.constraint(equalTo: success.bottomAnchor, constant: 200),
+            endButton.widthAnchor.constraint(equalToConstant: view.frame.width - 100),
+            endButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
         
     }
     
     private let successImage: UIImageView = {
+        // 성공시 띄워줄 이미지
         var image = UIImage(named: "dolphin")
         image = image?.withRenderingMode(.alwaysOriginal)
         let imageView = UIImageView(image: image)
@@ -46,6 +55,7 @@ class SubmitCompleteViewController: UIViewController {
     }()
     
     private let success:UITextView = {
+        // 퀴즈에 대한 해설이 있는 텍스트 뷰
        let success = UITextView()
         success.text = ""
         success.isEditable = false
@@ -58,5 +68,33 @@ class SubmitCompleteViewController: UIViewController {
         
         return success
     }()
+    
+    private let endButton: UIButton = {
+        // 해설을 전부 보고, 나가는 버튼
+        let button = UIButton()
+        button.setTitle("완료", for: .normal)
+        button.addTarget(self, action: #selector(clearButtonTapped), for: .touchUpInside)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.titleLabel?.textColor = .white
+        button.backgroundColor = UIColor.systemBrown
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // FIXME: func didClearQuizID did not work
+    
+    @objc private func clearButtonTapped() {
+             if let quiz = quiz {
+                 // 정답 맞췄을 때, 이런 식으로 건네주면 될 것 같다.
+                 delegate?.didClearQuizID(id: quiz.id)
+             }
+        for controller in self.navigationController!.viewControllers as Array {
+            if controller.isKind(of: StarViewController.self) {
+                    self.navigationController!.popToViewController(controller, animated: true)
+                    break
+                }
+        }
+        // reference: https://stackoverflow.com/questions/30003814/how-can-i-pop-specific-view-controller-in-swift
+         }
     
 }
